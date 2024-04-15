b8e6113a35bf12c16ff56284aa81b7a3c33dbd63
PalleSoffritte <153986623+PalleSoffritte@users.noreply.github.com>
2024-04-15 07:52:32 +0000

    matrice

--- X_BattleShip/Matrice.cpp
+++ X_BattleShip/Matrice.cpp
@@ -6,45 +6,83 @@ Mauro Marinelli 13/04/24
 #include <ctime>
 
 using namespace std;
+const int DIM= 6;
 
 class Matrice {
 
-    private:
-        int m[10][10];
+  private:
+    char m[DIM][DIM];
 
-    public:
-        Matrice() {
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    m[i][j]=0;
-                }
-                
+  public:
+    Matrice() {
+        for (int i=0; i<DIM; i++)
+            for (int j=0; j<DIM; j++)
+                m[i][j] = 97 + rand()%26;        
+    }
+
+    Matrice( char c ) {
+        for (int i=0; i<DIM; i++)
+            for (int j=0; j<DIM; j++)
+                m[i][j] = c;
+    }
+
+    char get( int x, int y ) {
+        return m[x][y];
+    }
+
+    void put( int x, int y, char c ) {
+        m[x][y] = c;
+    }
+
+    void stampa() {
+        cout << endl << " ------------------------------------------------------ " << endl;
+        for (int i=0; i<DIM; i++) {
+            for (int j=0; j<DIM; j++) {
+                printf("%c ", m[i][j]);
             }
-            
+            cout << endl;
         }
-        void stampa(){
-            cout<<endl<<"---------------------------------------------------------------------------"<<endl;
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    cout<<m[i][j]<<"\t";
-                }
-                cout<<endl;
-            }
-            cout<<endl<<"---------------------------------------------------------------------------"<<endl;
-        }
+        cout << endl << " ------------------------------------------------------ " << endl;
+    }
 
+    void bomb() {
+        int x = rand() % DIM;
+        int y = rand() % DIM;
+        m[x][y] = '*';
+    }
 
+    void placeHorizontalShip(int len) {
+        if (len<=0 || len>=DIM) return;
+        int x = rand() % (DIM);
+        int y = rand() % (DIM-len);
+        for (int i=0; i<len; i++) m[x][y+i] = 'O';
+    }
+
+    void placeVerticalShip(int len) {
+        if (len<=0 || len>=DIM) return;
+        int x = rand() % (DIM-len);
+        int y = rand() % (DIM);
+        for (int i=0; i<len; i++) m[x+i][y] = 'O';
+    }
 
 };
 
-int main(){
-    Matrice m1= Matrice();
-    m1.stampa();
-    
-    
-    return 0;
+int main() {
+    srand(time(NULL));
+    Matrice mappa = Matrice( '-' );
+    Matrice campo = Matrice( '.' );
+    campo.placeHorizontalShip(3);
+    campo.placeVerticalShip(4);
+    campo.placeVerticalShip(2);
+    campo.placeHorizontalShip(5);
+
+    // lancia 20 bombe a caso
+    for (int i=0; i<20; i++) {
+        int x = rand() % DIM;
+        int y = rand() % DIM;
+        if (campo.get(x,y)=='O') mappa.put(x,y,'*');
+    }
+
+    mappa.stampa();
+
 }
