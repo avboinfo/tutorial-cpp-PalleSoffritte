d5dd80bdafc73e7692ea828e8c1a496200a2316b
PalleSoffritte <153986623+PalleSoffritte@users.noreply.github.com>
2024-04-20 10:38:45 +0000

    BATTLEFIELD

--- X_BattleShip/BattleField2.cpp
+++ X_BattleShip/BattleField2.cpp
@@ -1,26 +1,28 @@
 /*
-Mauro Marinelli 13/04/24
+** BattleField.cpp una classe definita da noi che spupazza le matrici come nessuno mai
+** Sandro Gallo - 15/04/2024
 */
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
-const int DIM= 6;
-
-class Matrice {
+#include <iostream>
+using namespace std;
+const char SHIP ='X';
+const int DIM = 10;
+const char MISS = 'O';
+const char HIT= '*';
+const char VOID = '-';
+class BattleField {
 
   private:
     char m[DIM][DIM];
 
   public:
-    Matrice() {
+    BattleField() {
         for (int i=0; i<DIM; i++)
             for (int j=0; j<DIM; j++)
                 m[i][j] = 97 + rand()%26;        
     }
 
-    Matrice( char c ) {
+    BattleField( char c ) {
         for (int i=0; i<DIM; i++)
             for (int j=0; j<DIM; j++)
                 m[i][j] = c;
@@ -45,44 +47,18 @@ class Matrice {
         cout << endl << " ------------------------------------------------------ " << endl;
     }
 
-    void bomb() {
-        int x = rand() % DIM;
-        int y = rand() % DIM;
-        m[x][y] = '*';
-    }
-
     void placeHorizontalShip(int len) {
         if (len<=0 || len>=DIM) return;
         int x = rand() % (DIM);
         int y = rand() % (DIM-len);
-        for (int i=0; i<len; i++) m[x][y+i] = 'O';
+        for (int i=0; i<len; i++) m[x][y+i] = SHIP;
     }
 
     void placeVerticalShip(int len) {
         if (len<=0 || len>=DIM) return;
         int x = rand() % (DIM-len);
         int y = rand() % (DIM);
-        for (int i=0; i<len; i++) m[x+i][y] = 'O';
+        for (int i=0; i<len; i++) m[x+i][y] = SHIP;
     }
 
 };
-
-int main() {
-    srand(time(NULL));
-    Matrice mappa = Matrice( '-' );
-    Matrice campo = Matrice( '.' );
-    campo.placeHorizontalShip(3);
-    campo.placeVerticalShip(4);
-    campo.placeVerticalShip(2);
-    campo.placeHorizontalShip(5);
-
-    // lancia 20 bombe a caso
-    for (int i=0; i<20; i++) {
-        int x = rand() % DIM;
-        int y = rand() % DIM;
-        if (campo.get(x,y)=='O') mappa.put(x,y,'*');
-    }
-
-    mappa.stampa();
-
-}
---
+++ X_BattleShip/BattleShip2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+#include "BattleField2.cpp"
+class BattleShip{
+    private:
+    BattleField mappa;
+    BattleField campo;
+    public:
+    BattleShip(){
+    mappa= BattleField(VOID);
+    campo=BattleField(VOID);
+    campo.placeHorizontalShip(3);
+    campo.placeVerticalShip(4);
+    campo.placeVerticalShip(2);
+    campo.placeHorizontalShip(5);
+    }
+    void play(){
+        for (int i=0; i<20; i++) {
+        int x = rand() % DIM;
+        int y = rand() % DIM;
+        if(campo.get(x,y)==HIT) continue;
+        if (campo.get(x,y)==SHIP) {mappa.put(x,y,HIT); campo.put(x,y,HIT);}{
+        }else mappa.put(x,y,MISS);
+        mappa.stampa();
+        ask();
+        mappa.stampa();
+        campo.stampa();
+    }
+    
+    void ask(){
+        cout<<"inserisci le cordinate di riga e colonna dove sganciare la bomba"
+        int x,y;
+        cin>>x;
+        cin>>y;
+        if(campo.get(x,y)==HIT) continue;
+        if (campo.get(x,y)==SHIP) {mappa.put(x,y,HIT); campo.put(x,y,HIT);}
+        }else mappa.put(x,y,MISS);
+    }
